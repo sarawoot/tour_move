94d179d34dc37e24543e4645708adeeda444479a
sarawoot manmor <manmor2527@gmail.com>
2016-05-08 17:33:48 +0700

    redirect to admin index when admin login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,18 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError do |exception|
     redirect_to root_url, notice: 'ขออภัยคุณไม่มีสิทธิ์ใช้งาน'#exception.message#, status: :forbidden #HTTP 403
   end
+
+  private
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+    if resource.is_a?(User)
+      if resource.admin?
+        admin_home_index_url
+      elsif resource.user?
+        root_url
+      end
+    else
+      super
+    end
+  end
 end
